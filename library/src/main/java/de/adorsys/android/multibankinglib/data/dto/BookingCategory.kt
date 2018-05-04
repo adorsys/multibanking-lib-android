b096e6f7963a7e5a84d7e53618c5e3affdd09388
Meric Renksec <mre@adorsys.de>
2018-05-04 15:16:42 +0200

    3 - Created getBankAccounts, getBankAccesses, getBookings methods

---
+++ library/src/main/java/de/adorsys/android/multibankinglib/data/dto/BookingCategory.kt
@@ -0,0 +1,3 @@
+package <default> (/Users/meric/Development/multibanking-lib-android/library/src/test/java)
+
+data class BookingCategory(val bookingCategory: BookingCategory)
---
+++ library/src/main/java/de/adorsys/android/multibankinglib/data/dto/Contract.kt
@@ -0,0 +1,8 @@
+package <default> (/Users/meric/Development/multibanking-lib-android/library/src/test/java)
+
+data class Contract(val mandateReference: String = "",
+                    val hotline: Null = null,
+                    val logo: Null = null,
+                    val interval: String = "",
+                    val email: Null = null,
+                    val homepage: Null = null)
