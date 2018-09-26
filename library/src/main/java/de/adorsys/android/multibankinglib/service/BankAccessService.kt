5e6e342bae02bda381f63001fe272d9b0b894d1a
Drilon Reçica <drilonreqica@gmail.com>
2018-09-26 14:13:54 +0200

    Updated retrofit services

--- library/src/main/java/de/adorsys/android/multibankinglib/service/BankAccessService.kt
+++ library/src/main/java/de/adorsys/android/multibankinglib/service/BankAccessService.kt
@@ -2,7 +2,9 @@ package de.adorsys.android.multibankinglib.service
 
 import de.adorsys.android.multibankinglib.dto.BankAccess
 import retrofit2.Call
+import retrofit2.http.DELETE
 import retrofit2.http.GET
+import retrofit2.http.POST
 import retrofit2.http.Url
 
 interface BankAccessService {
@@ -10,5 +12,11 @@ interface BankAccessService {
     fun getBankAccesses(@Url resourcePath: String): Call<List<BankAccess?>?>
 
     @GET
-    fun getBankAccess(@Url resourcePath: String, id: String?): Call<BankAccess?>
+    fun getBankAccess(@Url resourcePath: String, id: String): Call<BankAccess?>
+
+    @POST
+    fun connectBankAccess(@Url resourcePath: String, id: String): Call<String?>
+
+    @DELETE
+    fun disconnectBankAccess(@Url resourcePath: String, id: String): Call<Boolean>
 }
